31bc6e0b88f995c9e4f7b6e9cb6ae9d7a7ffec9c
jabbate19 <josephabbateny@gmail.com>
2021-09-25 02:25:21 -0400

    Init

---
+++ project/DataFind.py
@@ -0,0 +1,228 @@
+import json
+
+def get_json(manual):
+    try:
+        return json.load(open(manual+'.json'))
+    except:
+        return None
+
+def table( data ):
+    file = ''
+    man = get_json( data[0] )
+    if not man:
+        return "Invalid Manual!"
+    if len(data)-1:
+        args_split = data[1].split('.')
+        size = len(args_split)
+        if size != 2:
+            return "Invalid Section Notation!"
+        section=[]
+        try:
+            section = [int(num) for num in args_split]
+        except ValueError:
+            return "Invalid Section Notation!"
+        if section[1]:
+            return specific_item(man['content'][section[0]-1]['subsections'][section[1]-1],data[1])
+        return specific_item(man['content'][section[0]-1],data[1])
+    else:
+        return items(man['content'])
+            
+
+def items(section, pre = '', start_tab=''):
+    string = ''
+    for sn in range(len(section)):
+        loc = ''
+        if pre:
+            pre_split = pre.split('.')
+            if int(pre_split[1]):
+                loc = pre+"."+str(sn+1)
+            else:
+                loc = pre_split[0]+'.'+str(sn+1)
+        else:
+            loc = str(sn+1)+".0"
+        string += start_tab + loc + ' ' + section[sn]['title'] + "\n"
+        #print(string)
+        if has_subsections(section[sn]):
+            string += items( section[sn]['subsections'], loc, start_tab+'\t')
+    return string
+
+def specific_item(section, pre):
+    string = ''
+    string += pre + ' ' + section['title'] + "\n"
+    if has_subsections(section):
+            string += items( section['subsections'], pre, '\t' )
+    return string
+
+def has_subsections(section):
+    try:
+        return len(section['subsections'])
+    except KeyError:
+        return False
+
+def read(data):
+    manual = data[0]
+    manual_json = get_json(manual)
+    if not manual_json:
+        return "Invalid Manual!"
+    section_num = data[1]
+    num_split = [ int(x) for x in section_num.split('.') ]
+    if validate_section( manual, section_num ):
+        out = ''
+        if len(num_split) == 2:
+            if num_split[1]:
+                section = manual_json['content'][num_split[0]-1]['subsections'][num_split[1]-1]
+                out += '__**'+section['title']+'**__'+"\n\n"
+                for line in section['content']:
+                    out += line + "\n"
+                for sub in section['subsections']:
+                    out += "\t" + "__**" + sub['title'] +'**__'+ "\n\n"
+                    for line in sub['content']:
+                        out += "\t" + line + "\n"
+            else:
+                section = manual_json['content'][num_split[0]-1]
+                out += '__**' + section['title'] + '**__' + "\n\n"
+                for line in section['content']:
+                    out += line + "\n"
+                for sub in section['subsections']:
+                    out += "\t" + '__**' + sub['title'] + '**__' + "\n\n"
+                    for line in sub['content']:
+                        out += "\t" + line + "\n"
+                    for subsub in sub['subsections']:
+                        out += "\t\t" + '__**' + subsub['title'] + '**__'  + "\n\n"
+                        for line in subsub['content']:
+                            out += "\t\t" + line + "\n"
+        else:
+            subsub = manual_json['content'][num_split[0]-1]['subsections'][num_split[1]-1]['subsections'][num_split[2]-1]
+            out += '__**' + subsub['title'] + '**__' + "\n\n"
+            for line in subsub['content']:
+                out += line + "\n"
+        return out
+    return "Invalid Section!"
+
+def validate_section(manual, section):
+    man = get_json(manual)
+    try:
+        num_split = [ int(x) for x in section.split('.') ]
+    except ValueError:
+        return False
+    size = len(num_split)
+    try:
+        if size >= 2 and size <= 3:
+            if not num_split[1]:
+                s = man['content'][num_split[0]-1]
+                return size == 2
+            s = man['content'][num_split[0]-1]['subsections'][num_split[1]-1]
+            if size == 3:
+                s2 = s['subsections'][num_split[2]-1]
+                return True
+            return True
+    except Exception as e:
+        return False
+
+def help(args):
+    with open('help.json') as f:
+        h = json.load(f)
+        out = ''
+        if args:
+            try:
+                section = h[args[0]]
+                out += '__**'+section['header']+'**__'+'\n'
+                out += section['syntax']+'\n'
+                for line in section['info']:
+                    out += line + '\n'
+            except:
+                out = "Command " + args[0] + " not found!"
+        else:
+            cmds = list(h.keys())
+            cmds.sort()
+            for cmd in cmds:
+                out += help([cmd])+'\n'
+    return out
+
+def rule( data ):
+    rules = get_json("rules")
+    out = ''
+    rule_to_section={
+        "C":"Competition",
+        "RG":"General Robot",
+        "RM":"Robot Mechanical",
+        "RE":"Robot Electrical",
+        "DS":"Driver Station",
+        "RS":"Robot Software",
+        "TE":"Team Scoring Element",
+        "I":"Inspection",
+        "S":"Safety",
+        "G":"General Game",
+        "GS":"Game Specific"
+    }
+    if data:
+        if containsNumber(data[0]):
+            rule_type = ''
+            try:
+                test = int(data[0][1])
+                rule_type = data[0][0]
+            except:
+                rule_type = data[0][:1]
+            try:
+                #print(rule_type)
+                rule_type = rule_to_section[rule_type]
+                #print(rule_type)
+                out += '**' + data[0] + '**' + '\n'
+                #print(rules[rule_type])
+                for line in rules[rule_type][data[0]]:
+                    out += line + '\n'
+            except:
+                out = "Invalid Rule!"
+        else:
+            try:
+                rule_type=rule_to_section[data[0]]
+                out += '__**' + rule_type + '**__' + '\n\n'
+                for rule in list(rules[rule_type].keys()):
+                    out += '**' + rule + '**' + '\n'
+                    for line in rules[rule_type][rule]:
+                        out += line + '\n'
+
+            except:
+                out = "Invalid Rule Section!"
+    else:
+        for section in list(rules.keys()):
+            out += "__**" + section + "**__" + '\n'
+            nums = list(rules[section].keys())
+            for r in range(len(nums)):
+                out += nums[r]
+                if r != len(list(rules[section].keys()))-1:
+                    out += ', '
+            out += "\n\n"
+    return out
+
+def containsNumber(value):
+    for character in value:
+        if character.isdigit():
+            return True
+    return False
+
+def cmd( data ):
+    global active
+    args = data[1:].split(' ')
+    main_arg = args.pop(0)
+    if main_arg == "table":
+        return table(args)
+    elif main_arg == "read":
+        return read(args)
+    elif main_arg == "rule":
+        return rule(args)
+    elif main_arg == "help":
+        return help(args)
+    elif main_arg == "ping":
+        return "Pong!"
+    elif main_arg == "exit" and __name__ == "__main__":
+        active = False
+        return 'Exiting...'
+    return "Invalid Command!"
+
+if __name__ == "__main__":
+    active = True
+    while active:
+        i = input("> ")
+        print(len(cmd(i)))
+#print("Final:", table('gm1 3.0'), sep='\n')
---
+++ project/__init__.py
@@ -0,0 +1,154 @@
+""""
+Copyright © Krypton 2021 - https://github.com/kkrypt0nn
+Description:
+This is a template to create your own discord bot in python.
+https://www.reddit.com/r/FTC/new/.json?count=20
+Version: 3.0
+"""
+
+import json
+import os
+import platform
+import sys
+import math
+import time
+import requests as r
+import DataFind
+import discord
+from discord.ext import tasks
+from discord.ext.commands import Bot
+import asyncio
+
+"""	
+Setup bot intents (events restrictions)
+For more information about intents, please go to the following websites:
+https://discordpy.readthedocs.io/en/latest/intents.html
+https://discordpy.readthedocs.io/en/latest/intents.html#privileged-intents
+
+
+Default Intents:
+intents.messages = True
+intents.reactions = True
+intents.guilds = True
+intents.emojis = True
+intents.bans = True
+intents.guild_typing = False
+intents.typing = False
+intents.dm_messages = False
+intents.dm_reactions = False
+intents.dm_typing = False
+intents.guild_messages = True
+intents.guild_reactions = True
+intents.integrations = True
+intents.invites = True
+intents.voice_states = False
+intents.webhooks = False
+
+Privileged Intents (Needs to be enabled on dev page), please use them only if you need them:
+intents.presences = True
+intents.members = True
+"""
+
+intents = discord.Intents.default()
+
+bot = Bot(command_prefix='>', intents=intents)
+#client = discord.Client()
+last_time_checked=time.time()
+
+# The code in this even is executed when the bot is ready
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user.name}")
+    print(f"Discord.py API version: {discord.__version__}")
+    print(f"Python version: {platform.python_version()}")
+    print(f"Running on: {platform.system()} {platform.release()} ({os.name})")
+    print("-------------------")
+    last_time_checked=time.time()
+    status_task.start()
+
+
+# Setup the game status task of the bot
+@tasks.loop(minutes=5.0)
+async def status_task():
+    global last_time_checked
+    await bot.change_presence(activity=discord.Game(">help"))
+    posts = r.get('https://www.reddit.com/r/FTC/new/.json', headers = {'User-agent': 'your bot 0.1'})
+    posts = posts.json()
+    posts = posts['data']['children']
+    memes = [ x for x in posts if ( x['data']['link_flair_text'] == 'Meme' and x['data']['created_utc'] > last_time_checked ) ]
+    memes.reverse()
+    for meme in memes:
+        embed=discord.Embed(title="New Meme on r/FTC", description="Behold, Meme!", color=0xe67e22, url="https://reddit.com" + meme['data']['permalink'])
+        embed.set_author(name=meme['data']['author'], url='https://reddit.com/u/'+meme['data']['author'])
+        embed.add_field(name=meme['data']['title'], value=":arrow_down: :arrow_down: :arrow_down:", inline=False)
+        url = meme['data']['url']
+        if url[-4:] == ".jpg" or url[-4:] == ".png" or url[-5:] == ".jpeg":
+            embed.set_image(url=meme['data']['url'])
+        for guild in bot.guilds:
+            channel = discord.utils.get(guild.text_channels, name="memes")
+            if channel:
+                await channel.send(embed=embed)
+    last_time_checked = math.floor(time.time())
+
+
+# Removes the default help command of discord.py to be able to create our custom help command.
+# The code in this event is executed every time someone sends a message, with or without the prefix
+@bot.event
+async def on_message(message):
+    # Ignores if a command is being executed by a bot or by the bot itself
+    if message.author == bot.user or message.author.bot:
+        return
+    if message.content[0]=='>':
+        out = DataFind.cmd(message.content)
+        pages = []
+        while len(out) > 5500:
+            pages.append(out[:4499]+'-')
+            out = out[4499:]
+        pages.append( out )
+        embs = []
+        for page in pages:
+            embed=discord.Embed(title="RoboTiger",description=message.content, color=0xe67e22)
+            while len(page) > 1024:
+                val = page[:1022]
+                embed.add_field(name="\u200b",value=val+'-', inline=False)
+                page = page[1022:]
+            embed.add_field(name="\u200b",value=page, inline=False)
+            embs.append( embed )
+        buttons = [u"\u23EA", u"\u2B05", u"\u27A1", u"\u23E9"] # skip to start, left, right, skip to end
+        current = 0
+        msg = await message.channel.send(embed=embs[current])
+        if len(pages) > 1:
+            for button in buttons:
+                await msg.add_reaction(button)
+                
+            while True:
+                try:
+                    reaction, user = await bot.wait_for("reaction_add", check=lambda reaction, user: user == message.author and reaction.emoji in buttons, timeout=60.0)
+
+                except asyncio.TimeoutError:
+                    return print("test")
+
+                else:
+                    previous_page = current
+                    if reaction.emoji == u"\u23EA":
+                        current = 0
+                        
+                    elif reaction.emoji == u"\u2B05":
+                        if current > 0:
+                            current -= 1
+                            
+                    elif reaction.emoji == u"\u27A1":
+                        if current < len(embs)-1:
+                            current += 1
+
+                    elif reaction.emoji == u"\u23E9":
+                        current = len(embs)-1
+
+                    for button in buttons:
+                        await msg.remove_reaction(button, message.author)
+
+                    if current != previous_page:
+                        await msg.edit(embed=embs[current])
+
+# Run the bot with the token
+bot.run(os.environ.get('DISCORD'))
